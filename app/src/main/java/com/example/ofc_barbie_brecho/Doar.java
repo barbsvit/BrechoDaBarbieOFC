fc5f3b15ba50b39bac4389017ef90f6b05806302
IsabelaESC <isabela.alves52@etec.sp.gov.br>
2023-09-04 22:18:03 -0300

    Atualização

--- app/src/main/java/com/example/ofc_barbie_brecho/Doar.java
+++ app/src/main/java/com/example/ofc_barbie_brecho/Doar.java
@@ -3,6 +3,7 @@ package com.example.ofc_barbie_brecho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,16 +14,30 @@ public class Doar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doar);
     }
+
     public void btnsobrenos(View s) {
         Intent intent = new Intent(this, QuemSomosNos.class);
         startActivity(intent);
     }
+
     public void btndoar(View d) {
         Intent intent = new Intent(this, Doar.class);
         startActivity(intent);
     }
+
     public void btnperfil(View p) {
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
+    }
+
+    public void btnvejamais(View e) {
+        Intent intent = new Intent(this, vejamais.class);
+        startActivity(intent);
+    }
+
+    public void mapa(View f) {
+        Uri location = Uri.parse("geo:-23.52004919329812, -46.7276384746785z=14");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+        startActivity(mapIntent);
     }
 }
---
+++ app/src/main/java/com/example/ofc_barbie_brecho/vejamais.java
@@ -0,0 +1,14 @@
+package com.example.ofc_barbie_brecho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class vejamais extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vejamais);
+    }
+}
